4150026fb36cc0cc442f7be391d071945f62b354
Islam <isxmad@gmail.com>
2026-01-13 15:37:17 +0500

    feat: About course page content styling

--- src/_pages/aboutCoursePage/ui/RestInfo/RestInfo.tsx
+++ src/_pages/aboutCoursePage/ui/RestInfo/RestInfo.tsx
@@ -3,12 +3,16 @@ import { FC } from "react";
 import { Box } from "@mui/material";
 import htmlParser from "html-react-parser";
 
+import { HTMLContent } from "@/shared/ui";
+
 import { Course } from "@/entities/course";
 
 type Props = Pick<Course, "explanation">;
 
 export const RestInfo: FC<Props> = ({ explanation }) => {
   return (
-    <Box sx={{ width: "100%", p: "4rem 1rem 0 0" }}>{explanation && htmlParser(explanation)}</Box>
+    <Box sx={{ width: "100%", p: "4rem 1rem 0 0" }}>
+      <HTMLContent value={explanation} />
+    </Box>
   );
 };
