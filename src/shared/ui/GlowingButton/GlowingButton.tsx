4450aa1d5f05fb1678c59e7457da25a894f54b60
Islam <isxmad@gmail.com>
2026-01-26 14:13:54 +0500

    feat: Lets play button redesign

--- src/shared/ui/GlowingButton/GlowingButton.tsx
+++ src/shared/ui/GlowingButton/GlowingButton.tsx
@@ -1,28 +1,18 @@
 import { FC } from "react";
 
 import { Button, ButtonProps } from "@mui/material";
-
-import { cn } from "@/shared/lib";
+import clsx from "clsx";
 
 import classes from "./classes.module.scss";
 
 export const GlowingButton: FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <Button
-      {...props}
-      sx={(theme) => ({
-        boxShadow:
-          "6px 0px 8px 0px rgba(255, 0, 0, 0.25), 0px 6px 8px 0px rgba(0, 255, 38, 0.25), 0px -6px 8px 0px rgba(43, 6, 255, 0.25),-6px 0px 8px 0px rgba(0, 222, 178, 0.25)",
-        "&:hover": {
-          boxShadow:
-            "15px 0px 15px 0px rgba(255, 0, 0, 0.25), 0px 15px 15px 0px rgba(0, 255, 38, 0.25), 0px -15px 15px 0px rgba(43, 6, 255, 0.25),-15px 0px 15px 0px rgba(0, 222, 178, 0.25)",
-        },
-        // "&::after": {
-        //   backgroundColor: theme.palette.primary.main,
-        // },
-      })}
-    >
-      <div className={classes.content}>{children}</div>
-    </Button>
+    <div className={classes.wrapper}>
+      <Button {...props} className={clsx(props.className, classes.button)}>
+        <div className={classes.content}>{children}</div>
+      </Button>
+      <div className={classes.mask}></div>
+      <div className={classes.shadow}></div>
+    </div>
   );
 };
