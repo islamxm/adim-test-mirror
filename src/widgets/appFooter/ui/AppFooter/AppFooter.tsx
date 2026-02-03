346ed45ab276029f5fa068da565294871ffc5096
Islam <isxmad@gmail.com>
2026-02-03 22:19:53 +0500

    feat: Dirty hide footer in lesson page

--- src/widgets/appFooter/ui/AppFooter/AppFooter.tsx
+++ src/widgets/appFooter/ui/AppFooter/AppFooter.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { usePathname } from "next/navigation";
+
 import { Box, Stack } from "@mui/material";
 
 import { Container } from "@/shared/ui/Container";
@@ -6,6 +8,10 @@ import { Container } from "@/shared/ui/Container";
 import { Copyright } from "../Copyright/Copyright";
 
 export const AppFooter = () => {
+  const pathname = usePathname();
+  if (pathname.includes("/main/")) {
+    return null;
+  }
   return (
     <Box sx={(theme) => ({ backgroundColor: theme.palette.gold.light })}>
       <Container>
